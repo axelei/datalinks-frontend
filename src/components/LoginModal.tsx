7c3f48baa1755f9508f3388e90bc19fbbcae3d90
Javier Olmo <olmogil@gmail.com>
2024-11-01 13:07:55 +0100

    added CSS

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,5 +1,5 @@
-import {ReactNode} from "react";
-import {Box, Modal, TextField} from "@mui/material";
+import React, {ReactNode} from "react";
+import {Box, FormControl, Modal, TextField} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import {modalStyle} from "../service/Common.ts";
@@ -7,76 +7,85 @@ import {SubmitHandler, useForm} from "react-hook-form";
 import {loadingOff, loadingOn} from "../redux/loadingSlice.ts";
 import {useDispatch} from "react-redux";
 import {Link} from "react-router-dom";
+import "./LoginModal.css";
 
-export default function LoginModal(props: { show: boolean, onClose: () => void }) : ReactNode | null {
+export default function LoginModal(props: { show: boolean, onClose: () => void }): ReactNode | null {
 
-    const handleClose = () => {
-        props.onClose();
+  const handleClose = () => {
+    props.onClose();
+  }
+
+  const dispatch = useDispatch();
+
+  type Inputs = {
+    username: string
+    password: string
+  }
+
+  const login = async (username: string, password: string): Promise<void> => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({username, password}),
+    };
+    const data = await fetch(import.meta.env.VITE_API + '/user/login', requestOptions);
+    if (data.ok) {
+      console.log('Login successful');
+      return data.json();
+    } else {
+      return Promise.reject('Login failed');
     }
+  }
 
-    const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (inputs: Inputs) => {
+    dispatch(loadingOn());
+    const result = login(inputs.username, inputs.password);
+    result.then((data) => {
+      console.log(data)
+    }).catch((error) => {
+      console.log(error);
+    }).finally(() => {
+      dispatch(loadingOff());
+    });
+  }
 
-    type Inputs = {
-        username: string
-        password: string
-    }
+  return (
+    <>
+      <Modal
+        aria-labelledby="unstyled-modal-title"
+        aria-describedby="unstyled-modal-description"
+        open={props.show}
+        onClose={handleClose}
+      >
+        <Box sx={modalStyle} className={"login-modal"}> {/* no le tengas alergia al css, te va a ahorrar problemas. Pasar styledObjects hace llorar al niño Jesús */}
+          <Typography>Log in</Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl fullWidth>  {/* Podrías utilizar Flexbox (display: flex) par esto, pero FormControl viene con material UI y ofrece ya ese estilo, mejor usar lo que ya tienes */}
+              <TextField label="Username" variant="outlined" {...register("username", {required: true})} />
+              {errors.username && <Typography color="error">Username is required</Typography>}
+            </FormControl>
 
-    const login = async (username : string, password: string) : Promise<void> => {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({username, password}),
-        };
-        const data = await fetch(import.meta.env.VITE_API + '/user/login', requestOptions);
-        if (data.ok) {
-            console.log('Login successful');
-            return data.json();
-        } else {
-            return Promise.reject('Login failed');
-        }
-    }
+            <FormControl fullWidth>
+              <TextField label="Password" variant="outlined" {...register("password", {required: true})} />
+              {errors.password && <Typography color="error">Password is required</Typography>}
+            </FormControl>
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<Inputs>()
-    const onSubmit: SubmitHandler<Inputs> = (inputs : Inputs) => {
-        dispatch(loadingOn());
-        const result = login(inputs.username, inputs.password);
-        result.then((data) => {
-            console.log(data)
-        }).catch((error) => {
-            console.log(error);
-        }).finally(() => {
-            dispatch(loadingOff());
-        });
-    }
+            <FormControl fullWidth className="hbox" >
+              <Button variant='contained' type="submit">Login</Button>
+              <Button variant='contained' onClick={handleClose}>Cancel</Button>
+            </FormControl>
 
-    return (
-        <>
-            <Modal
-                aria-labelledby="unstyled-modal-title"
-                aria-describedby="unstyled-modal-description"
-                open={props.show}
-                onClose={handleClose}
-            >
-                <Box sx={modalStyle}>
-                    <Typography>Log in</Typography>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <TextField label="Username" variant="outlined" {...register("username", { required: true })} /><br/>
-                        {errors.username && <Typography>Username is required<br/></Typography>}
-                        <TextField label="Password" variant="outlined" {...register("password", { required: true })} /><br/>
-                        {errors.password && <Typography>Password is required<br /></Typography>}
-                        <div>
-                            <Button variant='contained' type="submit">Login</Button>
-                            <Button variant='contained' onClick={handleClose}>Cancel</Button>
-                        </div>
-                        <p>Don't have an account? <Link to='/signup' onClick={props.onClose}>Sign up</Link></p>
-                    </form>
-                </Box>
-            </Modal>
-        </>
-);
-
+            <FormControl fullWidth><p>Don't have an account? <Link to='/signup' onClick={props.onClose}>Sign up</Link>
+            </p>
+            </FormControl>
+          </form>
+        </Box>
+      </Modal>
+    </>
+  )
 }
