890f62073b720ca6d445f0c5a6bbc7842aab72ee
Krusher <axelei@gmail.com>
2024-11-07 09:29:10 +0100

    Password change to component

---
+++ src/components/ChangePasswordComponent.tsx
@@ -0,0 +1,114 @@
+import {ReactNode, useEffect, useState} from "react";
+import {useTranslation} from "react-i18next";
+import {useDispatch} from "react-redux";
+import {useAppSelector} from "../hooks.ts";
+import {useLocation} from "react-router-dom";
+import {log} from "../service/Common.ts";
+import {loadingOff, loadingOn} from "../redux/loadingSlice.ts";
+import {SubmitHandler, useForm} from "react-hook-form";
+import InfoDialog from "./InfoDialog.tsx";
+import Typography from "@mui/material/Typography";
+import {Box, FormControl, TextField} from "@mui/material";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
+
+export default function ChangeUserComponent( props: { canEdit: boolean }) : ReactNode | null {
+
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const loggedUser = useAppSelector((state) => state.loggedUser);
+    const location = useLocation();
+
+    const [changePasswordValidationError, setChangePasswordValidationError] = useState<string>('');
+    const [showSuccess, setShowSuccess] = useState(false);
+
+    useEffect(() => {
+        log("ChangePasswordComponent useEffect");
+
+    }, [dispatch, location.pathname, loggedUser.user.level, loggedUser.user.name, t]);
+
+    const changePassword = async (inputs : ChangePasswordInputs) : Promise<string> => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'login-token': loggedUser.token },
+            body: inputs.password,
+        };
+        const data = await fetch(import.meta.env.VITE_API + '/user/passwordChange', requestOptions);
+        if (data.ok) {
+            return data.text();
+        } else {
+            return Promise.reject(data.text());
+        }
+    }
+
+    const sendPasswordChange = (inputs : ChangePasswordInputs) : void => {
+        dispatch(loadingOn());
+        const result = changePassword(inputs);
+        result.then((data: string) : void => {
+            log("Signup success: " + data);
+            setShowSuccess(true);
+        }).catch((error : Promise<string>) : void => {
+            error.then((data: string) : void => {
+                log("Password change error: " + data);
+                setChangePasswordValidationError(t("Unknown error"));
+            });
+        }).finally(() : void => {
+            dispatch(loadingOff());
+        });
+    }
+
+    type ChangePasswordInputs = {
+        password: string,
+        passwordAgain: string,
+    }
+
+    const validateForm = (inputs : ChangePasswordInputs) : Promise<string> => {
+        if (inputs.password !== inputs.passwordAgain) {
+            return Promise.reject(t("Passwords do not match"));
+        }
+        return Promise.resolve('');
+    }
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<ChangePasswordInputs>()
+    const onSubmit : SubmitHandler<ChangePasswordInputs> = (inputs : ChangePasswordInputs) : void => {
+        setChangePasswordValidationError('');
+        validateForm(inputs)
+            .then(() => {
+                sendPasswordChange(inputs);
+            }).catch((error) => {
+            setChangePasswordValidationError(error);
+        });
+    }
+
+    return (
+        <>
+            <Typography variant={"h3"}>{t("Change password")}</Typography>
+            <InfoDialog show={showSuccess} onClose={() => setShowSuccess(false)} text={t("Password changed successfully")} />
+            <Box hidden={!props.canEdit} className="passwordReset-form" component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FormControl>
+                        <TextField label={t("Password")} variant="outlined" type="password"
+                                   {...register("password", {required: true, min: 8})}
+                                   helperText={errors.password && t("Password must be 8 character minimum")}
+                                   error={!!errors.password}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <TextField label={t("Password confirmation")} variant="outlined" type="password"
+                                   {...register("passwordAgain", {required: true})}
+                                   helperText={errors.passwordAgain && t("Password confirmation is required")}
+                                   error={!!errors.passwordAgain}
+                        />
+                    </FormControl>
+                    <Typography color="error">{changePasswordValidationError}</Typography>
+                    <Button variant="contained" type="submit"><EditIcon/> {t("Save")}</Button>
+                </form>
+            </Box>
+        </>
+    );
+
+}
--- src/pages/UserComponent.tsx
+++ src/pages/UserComponent.tsx
@@ -7,13 +7,8 @@ import {fetchUser} from "../service/UserService.ts";
 import Typography from "@mui/material/Typography";
 import {log} from "../service/Common.ts";
 import {useAppSelector} from "../hooks.ts";
+import ChangePasswordComponent from "../components/ChangePasswordComponent.tsx";
 import {UserLevel} from "../model/user/UserLevel.ts";
-import EditIcon from "@mui/icons-material/Edit";
-import Button from "@mui/material/Button";
-import {SubmitHandler, useForm} from "react-hook-form";
-import {Box, FormControl, TextField} from "@mui/material";
-import {loadingOff, loadingOn} from "../redux/loadingSlice.ts";
-import InfoDialog from "../components/InfoDialog.tsx";
 
 export default function UserComponent() : ReactNode | null {
 
@@ -21,11 +16,8 @@ export default function UserComponent() : ReactNode | null {
     const dispatch = useDispatch();
     const loggedUser = useAppSelector((state) => state.loggedUser);
     const location = useLocation();
-
     const [user, setUser] = useState<User>(newUser());
-    const [changePasswordValidationError, setChangePasswordValidationError] = useState<string>('');
-    const [canEdit, setCanEdit] = useState(false);
-    const [showSuccess, setShowSuccess] = useState(false);
+    const [canEdit, setCanEdit] = useState<boolean>(false);
 
     useEffect(() => {
         log("UserComponent useEffect");
@@ -35,99 +27,22 @@ export default function UserComponent() : ReactNode | null {
             .then((data : User) => {
                 setUser({...data});
                 document.title = import.meta.env.VITE_SITE_TITLE + ' - ' + data.username;
-
-                setCanEdit(data.name === loggedUser.user.name || loggedUser.user.level === UserLevel.ADMIN);
-
+                if (data.username == loggedUser.user.username || loggedUser.user.level === UserLevel.ADMIN) {
+                    setCanEdit(true);
+                }
             }).catch(() => {
                 setUser({...newUser(), name: t("User not found")});
                 document.title = import.meta.env.VITE_SITE_TITLE + ' - ' + t("User not found");
         });
 
-    }, [dispatch, location.pathname, loggedUser.user.level, loggedUser.user.name, t]);
-
-    const changePassword = async (inputs : ChangePasswordInputs) : Promise<string> => {
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', 'login-token': loggedUser.token },
-            body: inputs.password,
-        };
-        const data = await fetch(import.meta.env.VITE_API + '/user/passwordChange', requestOptions);
-        if (data.ok) {
-            return data.text();
-        } else {
-            return Promise.reject(data.text());
-        }
-    }
-
-    const sendPasswordChange = (inputs : ChangePasswordInputs) : void => {
-        dispatch(loadingOn());
-        const result = changePassword(inputs);
-        result.then((data: string) : void => {
-            log("Signup success: " + data);
-            setShowSuccess(true);
-        }).catch((error : Promise<string>) : void => {
-            error.then((data: string) : void => {
-                log("Password change error: " + data);
-                setChangePasswordValidationError(t("Unknown error"));
-            });
-        }).finally(() : void => {
-            dispatch(loadingOff());
-        });
-    }
-
-    type ChangePasswordInputs = {
-        password: string,
-        passwordAgain: string,
-    }
-
-    const validateForm = (inputs : ChangePasswordInputs) : Promise<string> => {
-        if (inputs.password !== inputs.passwordAgain) {
-            return Promise.reject(t("Passwords do not match"));
-        }
-        return Promise.resolve('');
-    }
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<ChangePasswordInputs>()
-    const onSubmit : SubmitHandler<ChangePasswordInputs> = (inputs : ChangePasswordInputs) : void => {
-        setChangePasswordValidationError('');
-        validateForm(inputs)
-            .then(() => {
-                sendPasswordChange(inputs);
-            }).catch((error) => {
-            setChangePasswordValidationError(error);
-        });
-    }
+    }, [dispatch, location.pathname, loggedUser.user.level, loggedUser.user.name, loggedUser.user.username, t]);
 
     return (
         <>
-            <InfoDialog show={showSuccess} onClose={() => setShowSuccess(false)} text={t("Password changed successfully")} />
             <Typography variant="h2">{t("User")}: {user.username}</Typography>
-            <article className="passwordReset-form">
+            <article>
                 {user.name}
-                <Box hidden={!canEdit}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <FormControl>
-                            <TextField label={t("Password")} variant="outlined" type="password"
-                                       {...register("password", {required: true, min: 8})}
-                                       helperText={errors.password && t("Password must be 8 character minimum")}
-                                       error={!!errors.password}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <TextField label={t("Password confirmation")} variant="outlined" type="password"
-                                       {...register("passwordAgain", {required: true})}
-                                       helperText={errors.passwordAgain && t("Password confirmation is required")}
-                                       error={!!errors.passwordAgain}
-                            />
-                        </FormControl>
-                        <Typography color="error">{changePasswordValidationError}</Typography>
-                        <Button variant="contained" disabled={!canEdit} type="submit"><EditIcon/> {t("Save")}</Button>
-                    </form>
-                </Box>
+                <ChangePasswordComponent canEdit={canEdit} />
             </article>
         </>
     );
