03eb1aea4bae2d1d3032693fca6b51fc11e2cb64
Krusher <axelei@gmail.com>
2024-11-08 17:04:39 +0100

    File upload

--- src/components/EditorComponent.tsx
+++ src/components/EditorComponent.tsx
@@ -118,14 +118,11 @@ export default function EditorComponent( props : Props) : ReactNode | null {
                         ],
                     },
                     simpleUpload: {
-                        // The URL that the images are uploaded to.
-                        uploadUrl: 'http://example.com',
-                        // Enable the XMLHttpRequest.withCredentials property.
+                        uploadUrl: import.meta.env.VITE_API + '/file/upload',
                         withCredentials: true,
-                        // Headers sent along with the XMLHttpRequest to the upload server.
                         headers: {
                             // 'X-CSRF-TOKEN': 'CSRF-Token',
-                            Authorization: 'Bearer <JSON Web Token>'
+                            Authorization: 'Bearer ' + loggedUser.token,
                         }
                     },
                     style: {
