4ff4f76656dbcb9588c423699fe2c8a3308e510a
Krusher <axelei@gmail.com>
2024-11-19 19:29:27 +0100

    Categories

--- src/pages/CategoriesComponent.tsx
+++ src/pages/CategoriesComponent.tsx
@@ -1,5 +1,5 @@
-import {ChangeEvent, ReactNode, useEffect, useState} from "react";
-import {Category} from "../model/page/Category.ts";
+import React, {ChangeEvent, ReactNode, useEffect, useState} from "react";
+import {Category, newCategory} from "../model/page/Category.ts";
 import {useTranslation} from "react-i18next";
 import {formatDate, log} from "../service/Common.ts";
 import {useDispatch} from "react-redux";
@@ -7,7 +7,10 @@ import {loadingOff, loadingOn} from "../redux/loadingSlice.ts";
 import Typography from "@mui/material/Typography";
 import {
     Dialog,
-    DialogActions, DialogContent, DialogTitle,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Fab,
     Paper,
     Table,
     TableBody,
@@ -15,12 +18,18 @@ import {
     TableContainer,
     TableHead,
     TablePagination,
-    TableRow, TextField
+    TableRow,
+    TextField,
+    Tooltip
 } from "@mui/material";
 import {Link} from "react-router-dom";
 import Button from "@mui/material/Button";
 import DeleteIcon from '@mui/icons-material/Delete';
-import {fetchCategories} from "../service/CategoryService.ts";
+import {addCategory, deleteCategory, fetchCategories} from "../service/CategoryService.ts";
+import Box from "@mui/material/Box";
+import AddIcon from '@mui/icons-material/Add';
+import {useAppSelector} from "../hooks.ts";
+import {UserLevel} from "../model/user/UserLevel.ts";
 
 export default function CategoriesComponent(): ReactNode | null {
 
@@ -31,17 +40,29 @@ export default function CategoriesComponent(): ReactNode | null {
     const [page, setPage] = useState(0);
     const [pageSize, setPageSize] = useState(10);
     const [addCategoryOpen, setAddCategoryOpen] = useState(false);
+    const [deleteCategoryOpen, setDeleteCategoryOpen] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const loggedUser = useAppSelector((state) => state.loggedUser);
 
-    useEffect(() => {
+    const searchCategories = () => {
         dispatch(loadingOn());
-        fetchCategories(page, pageSize).then((categories) => {
+        const result = fetchCategories(page, pageSize);
+        result.then((categories) => {
             setCategories(categories);
         }).catch((error) => {
             log("Error fetching categories: " + error);
         }).finally(() => {
             dispatch(loadingOff());
         });
-    }, []);
+    }
+
+    useEffect(() => {
+        searchCategories();
+    }, [page, pageSize]);
+
+    useEffect(() => {
+        setIsAdmin(UserLevel[loggedUser.user.level].toString() == UserLevel.ADMIN.toString());
+    }, [loggedUser]);
 
     const handleChangePage = (_event: unknown, newPage: number) : void => {
         setPage(newPage);
@@ -51,8 +72,18 @@ export default function CategoriesComponent(): ReactNode | null {
         setPageSize(parseInt(event.target.value, 10));
     }
 
-    const deleteCategory = (name: string) => {
-
+    const handleDelete = () => {
+        dispatch(loadingOn());
+        const result = deleteCategory(categoryName, loggedUser.token);
+        result.then(() => {
+            searchCategories();
+        }).catch((error) => {
+            log("Error while deleting category: " + error);
+        }).finally(() => {
+            dispatch(loadingOff());
+            setCategoryName('');
+            setDeleteCategoryOpen(false);
+        });
     }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -62,17 +93,23 @@ export default function CategoriesComponent(): ReactNode | null {
     const handleSave = () => {
         if (categoryName) {
             dispatch(loadingOn());
-            const result = addCategory(categoryName);
-            result.then((data: Category) => {
-                setCategories([...categories, data]);
+            const result = addCategory(categoryName, loggedUser.token);
+            result.then(() => {
+                searchCategories();
                 setCategoryName('');
             }).catch((error) => {
                 log("Error while adding category: " + error);
             }).finally(() => {
                 setAddCategoryOpen(false);
+                setCategoryName('')
                 dispatch(loadingOff());
             });
         }
+    }
+
+    const openDeleteCategory = (name: string) => {
+        setCategoryName(name);
+        setDeleteCategoryOpen(true);
     }
 
     return (
@@ -97,7 +134,9 @@ export default function CategoriesComponent(): ReactNode | null {
                                     <Link to={'/category/' + row.name}>{row.name}</Link>
                                 </TableCell>
                                 <TableCell align="right">{formatDate(row.creationDate)}</TableCell>
-                                <TableCell align="right"><Button onClick={() => deleteCategory(row.name)}><DeleteIcon/></Button></TableCell>
+                                <TableCell align="right">
+                                    <Button hidden={!isAdmin} color="error" onClick={() => openDeleteCategory(row.name)}><DeleteIcon/></Button>
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
@@ -132,6 +171,35 @@ export default function CategoriesComponent(): ReactNode | null {
                     </Button>
                 </DialogActions>
             </Dialog>
+            <Dialog open={deleteCategoryOpen} onClose={() => {setDeleteCategoryOpen(false)}}>
+                <DialogTitle>{t("Delete category")}</DialogTitle>
+                <DialogContent>
+                    <Typography>{t("Are you sure you want to delete the category")} <b>{categoryName}</b>?</Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => {setDeleteCategoryOpen(false)}}>{t("Cancel")}</Button>
+                    <Button onClick={handleDelete} variant="contained" color="error">
+                        {t("Delete")}
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Box
+                sx={{
+                    position: 'fixed',
+                    top: 100,
+                    right: 16,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: 2,
+                    zIndex: 1300,
+                }}
+            >
+                <Tooltip title={t("Add category")} placement="left">
+                    <span><Fab color="primary" aria-label={t("Add category")} disabled={!isAdmin} onClick={() => setAddCategoryOpen(true)}>
+                        <AddIcon/>
+                    </Fab></span>
+                </Tooltip>
+            </Box>
         </>
     );
 }
--- src/service/CategoryService.ts
+++ src/service/CategoryService.ts
@@ -6,3 +6,33 @@ export const fetchCategories = async (page: number | null, pageSize: number | nu
         return Promise.reject(data.status);
     }
 }
+
+export const addCategory = async (category: string, token: string) => {
+    const data = await fetch(import.meta.env.VITE_API + '/category/add', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'text/plain',
+            'Authorization': 'Bearer ' + token,
+        },
+        body: category
+    });
+    if (data.ok) {
+        return data.json();
+    } else {
+        return Promise.reject(data.status);
+    }
+}
+
+export const deleteCategory = async (category: string, token: string) => {
+    const data = await fetch(import.meta.env.VITE_API + '/category/delete/' + category, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        },
+    });
+    if (data.ok) {
+        return data.json();
+    } else {
+        return Promise.reject(data.status);
+    }
+}
